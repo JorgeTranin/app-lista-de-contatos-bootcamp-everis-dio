0c376256bb4510533b1fec29c47236228902c4be
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2023-05-26 17:31:54 -0400

    initial commit

---
+++ app/src/main/java/com/jorgetranin/app_lista_de_contatos_bootcamp_everis_dio/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jorgetranin.app_lista_de_contatos_bootcamp_everis_dio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/test/java/com/jorgetranin/app_lista_de_contatos_bootcamp_everis_dio/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.jorgetranin.app_lista_de_contatos_bootcamp_everis_dio
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
