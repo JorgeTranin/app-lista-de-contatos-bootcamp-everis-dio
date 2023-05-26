3a4f6cace8533a788c2216751788afd788c907a5
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2023-05-26 18:02:20 -0400

    Implementação inicial do Layout do App

--- app/src/main/java/com/jorgetranin/app_lista_de_contatos_bootcamp_everis_dio/MainActivity.kt
+++ app/src/main/java/com/jorgetranin/app_lista_de_contatos_bootcamp_everis_dio/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.jorgetranin.app_lista_de_contatos_bootcamp_everis_dio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.jorgetranin.app_lista_de_contatos_bootcamp_everis_dio.databinding.ActivityListaDeContatosBinding
+import com.jorgetranin.app_lista_de_contatos_bootcamp_everis_dio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+
+        binding.fab.setOnClickListener {
+            val intent = Intent(this, ListaDeContatos::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
